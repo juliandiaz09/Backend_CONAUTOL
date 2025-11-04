87df4ee63e74bee95c66a55f1f21fb7438ad4213
JeimmyValderrama <86479248+JeimmyValderrama@users.noreply.github.com>
2025-11-04 18:11:17 -0500

    Eliminacion_flipbook

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,5 @@
 from app.models.proyecto import Proyecto
 from app.models.servicio import Servicio
 from app.models.chatbot import Chatbot
-from app.models.flipbook import Flipbook
 
-__all__ = ['Proyecto', 'Servicio', 'Chatbot', 'Flipbook']
+__all__ = ['Proyecto', 'Servicio', 'Chatbot']
